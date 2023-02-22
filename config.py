6915080f5e9b9bf637ef6add629a20d4da81a193
User <oleksejudin6@gmail.com>
2023-02-22 20:33:45 +0200

    something to main ,commands is coming

--- config.py
+++ config.py
@@ -15,9 +15,9 @@ class Settings:
         link_to_get_json_from_api: str = f'https://api.open-meteo.com/v1/forecast?latitude={self.latitude}&longitude={self.longitude}&hourly=temperature_2m'
         response = requests.get(link_to_get_json_from_api).text
         dictionary_with_info = json.loads(response)
-        all_time_info : list = dictionary_with_info.get('hourly').get("time")
-        all_time_temperature: list = dictionary_with_info.get('hourly').get("temperature_2m")
-        list_time_temperature: list = list(zip(all_time_temperature,all_time_info))
+        all_time : list = dictionary_with_info.get('hourly').get("time")
+        all_temperature: list = dictionary_with_info.get('hourly').get("temperature_2m")
+        list_time_temperature: list = list(zip(all_temperature,all_time))
         for i in list_time_temperature:
             print(i)
 
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from config import Settings,main
+from config import Settings
 
 
 
+while True:
+    list_with_command = ('1)Start', '2)Get info', '3)Get city by coordinates', '4)Get coordinates by city')
 
-if __name__=="__main__":
-    main()
+    for i in list_with_command:
+        print(i)
 
+    class Base(Settings):
 
+        command_number = int(input("Enter what to do:"))+1
 
-class Base(Settings):
-   pass
+        def get_command(self):
+            pass
+
