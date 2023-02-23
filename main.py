b02f32ad1458db96c38b7a6bd98a553fd07d4aa5
User <oleksejudin6@gmail.com>
2023-02-23 13:15:55 +0200

    something to main ,commands is coming

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from config import Settings
+from methods import Methods
+from methods import user
 
 
+class Session(Methods):
+    commands_tuple: tuple = ("1)Get temperature", "2)Get location", "3)Get coordinates")
+    for q in commands_tuple:
+        print(q)
+    number_of_command: int = int(input("Enter what to do:"))
 
-class Base(Settings):
 
-    command_number = int(input("Enter what to do:"))+1
-    pass
+    dict_with_commands: dict = {
+        1: user.get_weather(),
+        2: user.get_city(),
+        3: user.get_coord_from_city()
+    }
+    if not isinstance(number_of_command,int):
+        if not 0<number_of_command<len(dict_with_commands.keys()):
+            raise AttributeError("Enter correct value")
 
+    for i in dict_with_commands.keys():
+        if number_of_command == i:
+            dict_with_commands.get(i)
--- methods.py
+++ methods.py
@@ -5,20 +5,35 @@ import json
 geolocator = Nominatim(user_agent="MyApp")
 
 class Methods(Settings):
-    def get_wheater(self):
+    def get_weather(self):
         link_to_get_json_from_api: str = f'https://api.open-meteo.com/v1/forecast?latitude={self.latitude}&longitude={self.latitude}&hourly=temperature_2m'
         response = requests.get(link_to_get_json_from_api).text # dict with all wheater >>> json.loads(response)
         all_time_info: list = json.loads(response).get('hourly').get("time")
         all_temperature_info: list = json.loads(response).get('hourly').get("temperature_2m")
-        time_temperature_pair: list = list(zip(all_temperature_info,all_time_info))# list with pair in tuple time/temp (len 168)
-        print(time_temperature_pair)
-    def get_city(self):
+
+        # time_temperature_pair: list = list(zip(all_temperature_info,all_time_info)) list with pair in tuple time/temp (len 168)
+
+        print(f"Max temperature:{max(all_temperature_info)}\nMin temperature:{min(all_temperature_info)}\nAverage temparature:{sum(all_temperature_info) // 168}")
+        print("----Time is----")
+        for i in all_time_info[6:24:2]:
+            print(i)
+    def get_city(self):   # get city from coordinates (coordinates init in settings)
         city = geolocator.reverse(self.longitude+","+self.latitude) # city from coord
-        print(city)
-    def get_coord_from_city(self):
-        pass
+        if city == None :
+            print(f"Could not find the city from this ({self.latitude},{self.longitude}) coordinates")
+        else:
+            print(f"Location is:{city}")
+    def get_coord_from_city(self):  # get coordinates of city (city get in function)
+        city_input: str = input("Enter a city:")
+        if isinstance(city_input,str):
+            location = geolocator.geocode(city_input)
+            if location is not None:
+                latitude = location.latitude
+                longitude = location.longitude
+                print(f"The coordinates of {city_input} are: ({latitude}, {longitude})")
+            else:
+                print(f"Could not find the location of {city_input}")
+
 
 
 user = Methods()
-user.get_city()
-user.get_wheater()
