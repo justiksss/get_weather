c48d5b116b4c180108348a45794212520a0a86b2
User <oleksejudin6@gmail.com>
2023-02-23 15:39:42 +0200

    need review

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from methods import Methods
-from methods import user
+user = Methods()
 
 
 class Session(Methods):
-    commands_tuple: tuple = ("1)Get temperature", "2)Get location", "3)Get coordinates")
-    for q in commands_tuple:
-        print(q)
-    number_of_command: int = int(input("Enter what to do:"))
+
+    while True:
+        commands_tuple: tuple = ("1)Get info by coordinates", "2)Get location", "3)Get coordinates","4)Set new coordiantes")
+        for i in commands_tuple:
+            print(i)
+        @staticmethod
+        def main(number_of_command):
+            dict_with_commands: dict = {
+                1: user.get_weather,
+                2: user.get_city,
+                3: user.get_coord_from_city,
+                4: user.set_coordinates
+            }
+            dict_with_commands.get(number_of_command)()
 
 
-    dict_with_commands: dict = {
-        1: user.get_weather(),
-        2: user.get_city(),
-        3: user.get_coord_from_city()
-    }
-    if not isinstance(number_of_command,int):
-        if not 0<number_of_command<len(dict_with_commands.keys()):
-            raise AttributeError("Enter correct value")
+        main(int(input("Enter what to do:")))
 
-    for i in dict_with_commands.keys():
-        if number_of_command == i:
-            dict_with_commands.get(i)
+
+
+
+
--- methods.py
+++ methods.py
@@ -5,21 +5,26 @@ import json
 geolocator = Nominatim(user_agent="MyApp")
 
 class Methods(Settings):
+
+    def set_coordinates(self):
+        self.longitude = float(input("Enter new longitude"))
+        self.latitude = float(input("Enter new latitude"))
+
     def get_weather(self):
         link_to_get_json_from_api: str = f'https://api.open-meteo.com/v1/forecast?latitude={self.latitude}&longitude={self.latitude}&hourly=temperature_2m'
-        response = requests.get(link_to_get_json_from_api).text # dict with all wheater >>> json.loads(response)
+        response = requests.get(link_to_get_json_from_api).text # dict with all weather >>> json.loads(response)
         all_time_info: list = json.loads(response).get('hourly').get("time")
         all_temperature_info: list = json.loads(response).get('hourly').get("temperature_2m")
 
-        # time_temperature_pair: list = list(zip(all_temperature_info,all_time_info)) list with pair in tuple time/temp (len 168)
 
-        print(f"Max temperature:{max(all_temperature_info)}\nMin temperature:{min(all_temperature_info)}\nAverage temparature:{sum(all_temperature_info) // 168}")
-        print("----Time is----")
+        print("----Temperature for this time----")
         for i in all_time_info[6:24:2]:
             print(i)
+        print(f"Max temperature:{max(all_temperature_info)}\nMin temperature:{min(all_temperature_info)}\nAverage temparature:{sum(all_temperature_info) // 168}")
+
     def get_city(self):   # get city from coordinates (coordinates init in settings)
         city = geolocator.reverse(self.longitude+","+self.latitude) # city from coord
-        if city == None :
+        if city == None:
             print(f"Could not find the city from this ({self.latitude},{self.longitude}) coordinates")
         else:
             print(f"Location is:{city}")
@@ -33,7 +38,7 @@ class Methods(Settings):
                 print(f"The coordinates of {city_input} are: ({latitude}, {longitude})")
             else:
                 print(f"Could not find the location of {city_input}")
+        else:
+            raise ValueError("Enter a real city!")
 
 
-
-user = Methods()
